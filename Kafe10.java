f2673fb4ad01981ae0d8bbed60edc93b64e2f913
Lucky Satria <luckysatriautama2007@kamu.com>
2025-12-04 22:46:03 +0700

    Percobaan 3

--- Kafe10.java
+++ Kafe10.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kafe10 {
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
@@ -28,7 +30,30 @@ public class Kafe10 {
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
 
+
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
         Menu("Budi", true, "DISKON30");
+       
+        System.out.print("\nMasukkan pilihan menu (1-6): ");
+        int pilihanMenu = sc.nextInt();
+        
+        System.out.print("Masukkan banyak item: ");
+        int banyakItem = sc.nextInt();
+    
+        int totalBayar = hitungTotalHarga(pilihanMenu, banyakItem);
+      
+        System.out.println("Total yang harus dibayar: Rp " + totalBayar);
+        
+        sc.close();
+    }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaMenu = {15000, 20000, 22000, 12000, 18000};
+        int hargaSatuan = hargaMenu[pilihanMenu - 1];
+        int totalHarga = hargaSatuan * banyakItem;
+        return totalHarga;
     }
 }
